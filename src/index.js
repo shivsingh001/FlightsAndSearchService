33d7f9831060962a474e09aed17a3530173d6647
shivsingh001 <shivamkumar0346@gmail.com>
2024-01-06 22:04:19 +0530

    Setting the Basic server

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+
+const setupAndStartServer = async ()=>{
+
+    //created the express object
+    const app = express();
+    const PORT=3000;
+    app.listen(PORT, ()=>{
+        console.log(`Server started at ${PORT}`);
+    })
+
+}
+
+setupAndStartServer();
