1c3740b0e42e85e087a60d1768d635b8a3b54930
shivsingh001 <shivamkumar0346@gmail.com>
2024-01-14 05:08:29 +0530

    Added Time Check functionality

--- src/services/flight-service.js
+++ src/services/flight-service.js
@@ -1,5 +1,5 @@
 const {FlightRepository, AirplaneRepository} = require('../repository/index');
-
+const {compareTime} = require('../utils/helper');
 
 class FlightService {
 
@@ -9,7 +9,9 @@ class FlightService {
     }
     async createFlight(data) {
         try {
-           
+            if(!compareTime(data.arrivalTime,data.departureTime)){
+                throw{error : "Arrival time cannot be less departureTime"};
+            }
             const airplane = await this.airplaneRepository.getAirplane(data.airplaneId);
             const flight = await this.flightRepository.createFlight({
                 ...data,totalSeats:airplane.capacity  // Here we just destructure the data object and inserted the value of airpalne.capacity
