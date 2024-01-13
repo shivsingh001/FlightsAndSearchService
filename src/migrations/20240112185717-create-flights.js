b93a1f82780ba10302becdecef2cd322b4234387
shivsingh001 <shivamkumar0346@gmail.com>
2024-01-14 02:32:34 +0530

    Added flights model

--- src/migrations/20240112185717-create-flights.js
+++ src/migrations/20240112185717-create-flights.js
@@ -10,31 +10,43 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       flightNumber: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull:false,
+        unique:true
       },
       airplaneId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull:false
+
       },
       departureAirportId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull:false
+
       },
       arrivalAirportId: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull:false
       },
       arrivalTime: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        allowNull:false
       },
       departureTime: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        allowNull:false
       },
       price: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull:false
       },
       boardingGate: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+       // allowNull:false
       },
       totalSeats: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        allowNull:false
       },
       createdAt: {
         allowNull: false,
