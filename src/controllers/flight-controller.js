19b9ae3a9a40360548d105f28dfef96435189f60
shivsingh001 <shivamkumar0346@gmail.com>
2024-01-15 04:07:59 +0530

    Implemented the Error Codes in Flight Search Service

--- src/controllers/flight-controller.js
+++ src/controllers/flight-controller.js
@@ -1,5 +1,5 @@
 const {FlightService} = require('../services/index');
-
+const {SuccessCodes } = require('../utils/error-codes')
 
 const flightService = new FlightService();
 
@@ -15,7 +15,7 @@ const create = async (req, res) =>{
             price: req.body.price
         }
         const flight = await flightService.createFlight(flightRequestData);
-        return res.status(201).json({
+        return res.status(SuccessCodes.CREATED).json({
             data:flight,
             success: true,
             message : 'Successfully created a flight' 
@@ -35,7 +35,7 @@ const create = async (req, res) =>{
 const getAll = async (req, res) =>{
     try {
         const response = await flightService.getAllFlightData(req.query);
-        return res.status(200).json({
+        return res.status(SuccessCodes.OK).json({
             data:response,
             success: true,
             error : {},
